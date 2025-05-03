952f3a851f303bd1f34b05be2d0e0629bf4cd7ed
ranaart <ranakei01@gmail.com>
2025-05-03 15:51:37 +0700

    tes1

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -8,35 +8,35 @@
                         <div class="logo logo-width-1 wow fadeIn animated">
                             <a href="index.html"><img src="{{ asset('assets/imgs/logo/logo.png')}}" alt="logo"></a>
                         </div>
-                        <h5 class="mt-20 mb-10 fw-600 text-grey-4 wow fadeIn animated">Contact</h5>
+                        <h5 class="mt-20 mb-10 fw-600 text-grey-4 wow fadeIn animated">Kontak Kami</h5>
                         <p class="wow fadeIn animated">
-                            <strong>Address: </strong>Tetouan Shore
+                            <strong>Alamat: </strong>Tetouan Shore
                         </p>
                         <p class="wow fadeIn animated">
-                            <strong>Phone: </strong>+212 682860421
+                            <strong>Telepon: </strong>+021 682860421
                         </p>
                         <p class="wow fadeIn animated">
-                            <strong>Email: </strong>yusufisawi@gmail.com
+                            <strong>Email: </strong>moka@gmail.com
                         </p>
                     </div>
                 </div>
                 <div class="col-lg-2 col-md-3">
-                    <h5 class="widget-title wow fadeIn animated">About</h5>
+                    <h5 class="widget-title wow fadeIn animated">Tentang</h5>
                     <ul class="footer-list wow fadeIn animated mb-sm-5 mb-md-0">
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Delivery Information</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                        <li><a href="#">Terms &amp; Conditions</a></li>
-                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Tentang Kami</a></li>
+                        <li><a href="#">Informasi Pengiriman</a></li>
+                        <li><a href="#">Kebijakan Privasi</a></li>
+                        <li><a href="#">Persyaratan &amp; Ketentuan</a></li>
+                        <li><a href="#">Kontak Kami</a></li>
                     </ul>
                 </div>
                 <div class="col-lg-2  col-md-3">
-                    <h5 class="widget-title wow fadeIn animated">My Account</h5>
+                    <h5 class="widget-title wow fadeIn animated">Akun Saya</h5>
                     <ul class="footer-list wow fadeIn animated">
-                        <li><a href="my-account.html">My Account</a></li>
-                        <li><a href="#">View Cart</a></li>
-                        <li><a href="#">Track My Order</a></li>
-                        <li><a href="#">Order</a></li>
+                        <li><a href="my-account.html">Akun Saya</a></li>
+                        <li><a href="#">Lihat Keranjang</a></li>
+                        <li><a href="#">Lacak Pesanan Saya</a></li>
+                        <li><a href="#">Pesanan</a></li>
                     </ul>
                 </div>
 
