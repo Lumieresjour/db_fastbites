69da22cecadf7f426a998a294e72069b4f87b9a3
ranaart <ranakei01@gmail.com>
2025-05-04 16:38:12 +0700

    sidebar 1.0.1

--- resources/views/livewire/success.blade.php
+++ resources/views/livewire/success.blade.php
@@ -5,12 +5,12 @@
                     <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
                 </svg>
             </div>
-            <h1 class="text-2xl font-bold text-center mt-4">Payment Success</h1>
+            <h1 class="text-2xl font-bold text-center mt-4">Pembayaran Sukses!</h1>
 
-            <p class="text-center mt-2">Thank you, {{ucfirst($customer->name)}}.</p>
-            <p class="text-center mt-2">Your payment has been processed successfully.</p>
+            <p class="text-center mt-2">Terimakasih, {{ucfirst($customer->name)}}.</p>
+            <p class="text-center mt-2">Pembayaran Anda telah berhasil diproses.</p>
             <div class="mt-6 flex justify-center">
-                <a href="{{ route('home') }}" class="btn btn-default">Go back to shopping</a>
+                <a href="{{ route('home') }}" class="btn btn-default">Kembali berbelanja</a>
             </div>
     </div>
 </x-app-layout>
