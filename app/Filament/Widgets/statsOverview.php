dfc069025f1e63266774c31945fe6d3b8d4f3b90
Lumieresjour <hanasaki532@gmail.com>
2025-06-09 10:34:17 +0700

    translate

--- app/Filament/Widgets/statsOverview.php
+++ app/Filament/Widgets/statsOverview.php
@@ -14,16 +14,16 @@ class statsOverview extends BaseWidget
         $totalSales = Order::where('status', 'completed')->sum('total');
         $totalSalesLast30Days = Order::where('status', 'completed')->where('created_at', '>=', now()->subDays(30))->sum('total');
         return [
-            Card::make('Last 30 Days', '$'.$totalSalesLast30Days)
-            ->description('Total sales for the last 30 days')
+            Card::make('30 Hari Terakhir', 'Rp'.$totalSalesLast30Days)
+            ->description('Total penjualan selama 30 hari terakhir')
             ->color('success')
             ->icon('heroicon-o-currency-dollar'),
-            Card::make('Total Sales', '$'.$totalSales)
-            ->description('Total income from completed orders')
+            Card::make('Total Penjualan', 'Rp'.$totalSales)
+            ->description('Total pendapatan dari pesanan yang diselesaikan')
             ->color('success')
             ->icon('heroicon-o-currency-dollar'),
-            Card::make('Unshipped Orders', $unshippedOrders)
-              ->description('Orders that have not been shipped yet')
+            Card::make('Pesanan yang Belum Dikirim', $unshippedOrders)
+              ->description('Pesanan yang belum dikirim')
               ->color('danger')
               ->icon('heroicon-o-inbox'),
         ];
