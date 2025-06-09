b1a138cc9e9f98732f11f6bfbd95068bb3ca9fce
Lumieresjour <hanasaki532@gmail.com>
2025-06-09 11:15:09 +0700

    pengantian dashboard

--- app/Filament/Widgets/statsOverview.php
+++ app/Filament/Widgets/statsOverview.php
@@ -15,15 +15,17 @@ class statsOverview extends BaseWidget
         $totalSalesLast30Days = Order::where('status', 'completed')->where('created_at', '>=', now()->subDays(30))->sum('total');
         return [
             Card::make('30 Hari Terakhir', 'Rp'.$totalSalesLast30Days)
+            ->value('Rp'.number_format($totalSalesLast30Days, 0, ',', '.'))
             ->description('Total penjualan selama 30 hari terakhir')
             ->color('success')
             ->icon('heroicon-o-currency-dollar'),
             Card::make('Total Penjualan', 'Rp'.$totalSales)
+            ->value('Rp'.number_format($totalSales, 0, ',', '.'))
             ->description('Total pendapatan dari pesanan yang diselesaikan')
             ->color('success')
             ->icon('heroicon-o-currency-dollar'),
             Card::make('Pesanan yang Belum Dikirim', $unshippedOrders)
-              ->description('Pesanan yang belum dikirim')
+              ->description('Orders that have not been shipped yet')
               ->color('danger')
               ->icon('heroicon-o-inbox'),
         ];
